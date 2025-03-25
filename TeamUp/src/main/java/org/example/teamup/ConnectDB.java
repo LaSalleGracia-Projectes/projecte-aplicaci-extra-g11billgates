70a7079ba0d644a94d2c44fd5951a06539bd2c8c
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2025-03-25 15:48:13 +0100

    Patron singleton creado creado en ConnectDB.java

--- TeamUp/src/main/java/org/example/teamup/ConnectDB.java
+++ TeamUp/src/main/java/org/example/teamup/ConnectDB.java
@@ -3,5 +3,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class ConnectDB {
-
+    private static Connection instance;
+    private ConnectDB() {}
+    public static Connection getInstance() {
+        if (instance == null) {
+            try {
+                String url = "jdbc:mysql://localhost:3306/testdb";
+                String user = "root";
+                String password = "";
+                instance = DriverManager.getConnection(url, user,
+                        password);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return instance;
+    }
 }
