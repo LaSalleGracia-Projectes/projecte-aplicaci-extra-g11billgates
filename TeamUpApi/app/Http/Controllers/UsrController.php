d8664c2ac3443d8fd160fdbd7467081b39f64731
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2025-03-25 17:42:44 +0100

    feat funcion de index UsrController añadida para comprobar el funcionamiento de Login y register

--- TeamUpApi/app/Http/Controllers/UsrController.php
+++ TeamUpApi/app/Http/Controllers/UsrController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class UsrController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Countries retrieved successfully',
+            'data' => Usuario::all()
+        ], 200);
+    }
 }
