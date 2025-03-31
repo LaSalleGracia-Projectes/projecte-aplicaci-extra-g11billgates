c3a82a8b6c427c1251595aac98198934e5580653
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2025-03-31 14:33:12 +0200

    feat: modelo de usuario completado menos la relacion con mensaje

--- TeamUpApi/app/Models/usuario.php
+++ TeamUpApi/app/Models/usuario.php
@@ -6,8 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Usuario extends Model
 {
     use HasFactory;
+    use HasApiTokens;
+
+    protected $fillabe = [
+        'IDUsuario',
+        'Nombre',
+        'Correo',
+        'Contraseña',
+        'FotoPerfil',
+        'Edad',
+        'Region'
+    ];
+    protected $hidden = [
+        'Contraseña'
+    ];
+    protected function cast(): array
+    {
+        return [
+            'Contraseña' => 'hashed'
+        ];
+    }
+    public function matchUsers()
+    {
+        return $this->hasMany(MatchUsers::class);
+    }
+    public function juego(): BelongsToMany
+    {
+        return $this->belongsToMany(Juego::class)->withPivot('Estadisticas', 'Preferencias', 'NivelElo');
+    }
 }
