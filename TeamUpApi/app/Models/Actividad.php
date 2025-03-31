596e1daa8aa225ba2cc2c9e6b3dfe533aada9eb2
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2025-03-31 14:21:59 +0200

    Hasfactory e importaciones añadidas a todos los modelos

--- TeamUpApi/app/Models/Actividad.php
+++ TeamUpApi/app/Models/Actividad.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Actividad extends Model
 {
-    //
+    use HasFactory;
 }
--- TeamUpApi/app/Models/JuegoUsuario.php
+++ TeamUpApi/app/Models/JuegoUsuario.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class JuegoUsuario extends Model
 {
-    //
+    use HasFactory;
 }
