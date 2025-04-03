a7a832fc7fc7a1dc0ef7fa638fb38fd52c9d5ff3
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2025-04-03 17:57:10 +0200

    feat: SceneController creado

---
+++ TeamUp/src/main/java/org/example/teamup/SceneController.java
@@ -0,0 +1,13 @@
+package org.example.teamup;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+public class SceneController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+}
