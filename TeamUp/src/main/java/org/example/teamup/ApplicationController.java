1ebef23a0e696ae0893916f0f67b2a6e48d2c16e
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2025-04-03 18:11:52 +0200

    refactor: funciones de cambios de escenas puestas en el ApplicationController ya que solo un controlador puede etar asignado a una vista como máxinmo

--- TeamUp/src/main/java/org/example/teamup/ApplicationController.java
+++ TeamUp/src/main/java/org/example/teamup/ApplicationController.java
@@ -1,18 +1,29 @@
 package org.example.teamup;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
 
 import java.io.IOException;
 
-public class SceneController {
+public class ApplicationController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Comprob");
+    } //hacer el proceso de login
+
+
 
     public void switchToLogin(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("login-view.fxml"));
@@ -20,12 +31,10 @@ public class SceneController {
         scene = new Scene(root);
         stage.show();
     }
-    public void switchToRegister(ActionEvent event)
-    {
+    public void switchToRegister(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("register-view.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.show();
     }
-
 }
