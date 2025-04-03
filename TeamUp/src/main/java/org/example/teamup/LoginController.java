ff13dd553f695789e7d22131e8b7711ce9017e7e
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2025-04-03 17:43:02 +0200

    refactor: cambiados los nombres de las vistas de las que tiene javafx por defecto a login y register

---
+++ TeamUp/src/main/java/org/example/teamup/LoginController.java
@@ -0,0 +1,20 @@
+package org.example.teamup;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.Scene;
+
+public class LoginController {
+    @FXML
+    private Label welcomeText;
+
+
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Comprob");
+    } //hacer el proceso de login
+    protected void cambiarRegister() {
+        Scene scene  = new Scene()
+    }
+}
