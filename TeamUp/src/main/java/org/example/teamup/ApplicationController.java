8e9ba29335705774dc18a532e8e30b24a9787f73
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2025-04-03 18:33:06 +0200

    feat: vista register creada

--- TeamUp/src/main/java/org/example/teamup/ApplicationController.java
+++ TeamUp/src/main/java/org/example/teamup/ApplicationController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -15,6 +16,7 @@ public class ApplicationController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    private ComboBox<String> region;
     @FXML
     private Label welcomeText;
 
@@ -23,18 +25,29 @@ public class ApplicationController {
         welcomeText.setText("Comprob");
     } //hacer el proceso de login
 
+    @FXML
+    public void initialize(){
+        region.getItems().addAll("Europa", "Norteamérica", "Sudamerica", "Asia", "Africa", "Oceania");
+    }
 
-
+    @FXML
     public void switchToLogin(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("login-view.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.show();
     }
+
+    @FXML
     public void switchToRegister(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("register-view.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.show();
+    }
+
+    @FXML
+    private void selectRegion(ActionEvent event) {
+        String selected = region.getValue();
     }
 }
