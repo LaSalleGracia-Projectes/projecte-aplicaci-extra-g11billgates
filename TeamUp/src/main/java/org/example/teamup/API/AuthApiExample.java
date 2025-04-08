4511fbb67c250302cc19ddece4de5120733c7947
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2025-04-08 14:25:27 +0200

    feat: pachkage de api creado

---
+++ TeamUp/src/main/java/org/example/teamup/API/AuthApiExample.java
@@ -0,0 +1,11 @@
+package org.example.teamup.API;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class AuthApiExample {
+
+}
