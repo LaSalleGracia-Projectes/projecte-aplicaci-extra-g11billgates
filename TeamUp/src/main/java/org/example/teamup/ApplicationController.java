dd28c3c20e97655d2823dd5c585448e3655badb6
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2025-04-08 17:09:31 +0200

    feat: funcion handle login creada en el controlador para hacer la request de login a la api

--- TeamUp/src/main/java/org/example/teamup/ApplicationController.java
+++ TeamUp/src/main/java/org/example/teamup/ApplicationController.java
@@ -1,5 +1,6 @@
 package org.example.teamup;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,6 +9,8 @@ import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.concurrent.Task;
+import org.example.teamup.API.AuthApiExample;
 
 import java.io.IOException;
 
@@ -20,8 +23,8 @@ public class ApplicationController {
     private Label welcomeText;
     @FXML
     private TextField emailField;
-    //@FXML
-    //private PasswordField passwordField;
+    @FXML
+    private PasswordField passwordField;
     @FXML
     private Hyperlink enlace_register;
     @FXML
@@ -43,6 +46,26 @@ public class ApplicationController {
         }
 
     }
+    @FXML
+    public void handleLogin(ActionEvent event){
+        String email = emailField.getText();
+        String password = passwordField.getText();
+        // Ejecutar la llamada a la API en un hilo aparte
+        Task<Void> task = new Task<Void>() {
+            protected Void call() throws Exception {
+                try {
+                    // Llamada a la función de login de AuthApiExample
+                    AuthApiExample.login(email, password);
+                    Platform.runLater(() -> welcomeText.setText("Inicio de sesión exitoso."));
+                } catch (IOException ex) {
+                    Platform.runLater(() -> welcomeText.setText("Error en login: " + ex.getMessage()));
+                }
+                return null;
+            }
+        };
+        new Thread(task).start();
+    }
+
 
 
 }
