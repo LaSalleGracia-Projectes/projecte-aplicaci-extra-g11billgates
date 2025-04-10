f597931c614f5c5d9b0a60e5c2bafa2b59a5c258
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2025-04-10 17:28:29 +0200

    feat: libreria de bootstrap añadida en la llamada al main

--- TeamUp/src/main/java/org/example/teamup/HelloApplication.java
+++ TeamUp/src/main/java/org/example/teamup/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 //imports para la conexión con la base de datos de mysql
 
@@ -18,6 +19,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
+        scene.getStylesheets().add(getClass().getResource("/css/custom-bootstrapfx.css").toExternalForm());
         stage.setTitle("TeamUp");
         stage.setScene(scene);
         stage.show();
