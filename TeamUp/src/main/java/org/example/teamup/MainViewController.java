67b6e27ac5ceda7b4cd17ed93d20f7dbfd9ac648
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2025-04-29 16:29:44 +0200

    style: imagen de ejemplo añadida

--- TeamUp/src/main/java/org/example/teamup/MainViewController.java
+++ TeamUp/src/main/java/org/example/teamup/MainViewController.java
@@ -1,6 +1,8 @@
 package org.example.teamup;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.control.Label;
 
@@ -8,6 +10,16 @@ public class MainViewController {
 
     @FXML
     private StackPane contentArea;
+
+    @FXML
+    private ImageView bienvenidaImage;
+
+    @FXML
+    public void initialize() {
+        Image image = new Image(getClass().getResource("/org/example/teamup/images/harambe.png").toExternalForm());
+        bienvenidaImage.setImage(image);
+    }
+
 
     // Ya no necesitamos initialize()
 }
