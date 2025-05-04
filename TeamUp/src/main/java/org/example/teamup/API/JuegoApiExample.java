89f8b27cc27df3f15f04dbec0069d62ace6f6b91
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2025-05-04 17:39:42 +0200

    feat: funcion para añadir juego implementada en el backend de java

--- TeamUp/src/main/java/org/example/teamup/API/JuegoApiExample.java
+++ TeamUp/src/main/java/org/example/teamup/API/JuegoApiExample.java
@@ -54,5 +54,10 @@ public class JuegoApiExample {
         conn.disconnect();
         return response.toString();
     }
+    public static String addJuego(int juegoId, String bearerToken) throws IOException {
+        String url = "http://127.0.0.1:8000/api/usuario/juego";
+        String json = String.format("{\"juego_id\": %d}", juegoId);
+        return sendRequest("POST", url, bearerToken, json);
+    }
 
 }
